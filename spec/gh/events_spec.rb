b772a081baeae410b4332411e87f3edb936c6521
Gareth Rees <gareth@garethrees.co.uk>
2020-01-11 17:36:42 +0000

    Minor refactor of spec/gh/events_spec
    * Add a describe block for the `.typeof` method so that its clearer
      when we add more method specs in the future
    * Minor line-spacing improvements

--- spec/gh/events_spec.rb
+++ spec/gh/events_spec.rb
@@ -5,13 +5,17 @@ RSpec.describe GH::Events do
     expect(GH::Events::VERSION).not_to be nil
   end
 
-  path = File.expand_path(File.join(%w(.. .. fixtures)), __FILE__)
-  Dir.glob('*.json', base: path).sort.each do |file|
-    name = File.basename(file, '.json')
-    it "detects events of type #{name}" do
-      payload = JSON.parse(File.read(File.join(path, file)))
-      result = GH::Events.typeof(payload)
-      expect(result).to eq(name.to_sym)
+  describe '.typeof' do
+    path = File.expand_path(File.join(%w(.. .. fixtures)), __FILE__)
+
+    Dir.glob('*.json', base: path).sort.each do |file|
+      name = File.basename(file, '.json')
+
+      it "detects events of type #{name}" do
+        payload = JSON.parse(File.read(File.join(path, file)))
+        result = GH::Events.typeof(payload)
+        expect(result).to eq(name.to_sym)
+      end
     end
   end
 end
